cd8b46f17cf5e84bc3045c571497ee7aae046242
Iara Isleño <iara_joana@hotmail.com>
2020-07-27 20:51:08 -0300

    ejercicio definitivo

--- excercise-one-one/src/ExcersiceOneOne.java
+++ excercise-one-one/src/ExcersiceOneOne.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ExcersiceOneOne {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         System.out.println("Aplicación para calcular el area de un triangulo rectangulo");
 
@@ -20,19 +19,6 @@ public class ExcersiceOneOne {
         altura = keyboard.nextInt();
 
         area = (base * altura) /2;
-=======
-        int catetoA, catetoB, area;
-
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.print("Ingrese valor del primer cateto: ");
-        catetoA = keyboard.nextInt();
-
-        System.out.print("Ingrese valor del segundo cateto: ");
-        catetoB = keyboard.nextInt();
-
-        area = (catetoA * catetoB) /2;
->>>>>>> e3a37fa64b3c674221c65e8dc687c691cbad4535
 
         System.out.println("El area del triangulo es " + area);
     }
